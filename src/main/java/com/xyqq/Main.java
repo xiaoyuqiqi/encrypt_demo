4daf349ee68c22f844aa24f656677ad7659f94e7
lizhiqi <1029888402@qq.com>
2024-02-27 10:39:05 +0800

    feat(init): 初始化项目

---
+++ src/main/java/com/xyqq/Main.java
@@ -0,0 +1,16 @@
+package com.xyqq;
+
+/**
+ * ClassName: ${NAME}
+ * Package: com.xyqq
+ * description:
+ *
+ * @author lzq
+ * @version 1.0
+ * @date ${DATE} ${TIME}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
